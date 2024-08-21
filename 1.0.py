853b2988793204d07f1cdc11f3de245d01e96cd1
billy-juicy <libertyeternity00@gmail.com>
2024-08-21 16:06:33 +0300

    add new lines

--- 1.0.py
+++ 1.0.py
@@ -4,15 +4,34 @@ digits = '0123456789'
 lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
 uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = '!#$%&*+-=?@^_'
+
 chars = ''
 
-def password_main():
-    input('Какое количество паролей сгенерировать? ').isdigit()
-    input('Какова длина будущего пароля? ').isdigit()
-    input('Включать ли цифры 0123456789? (Да/Нет) ').capitalize()
-    input('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? (Да/Нет) ').capitalize()
-    input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? (Да/Нет) ').capitalize()
-    input('Включать ли символы !#$%&*+-=?@^_? (Да/Нет) ').capitalize()
-    input('Исключать ли неоднозначные символы il1Lo0O? (Да/Нет) ').capitalize()
+num = int(input('Введите количество паролей для генерации: '))
+length = int(input('Введите длину пароля: '))
+digit = input('Включить в пароль цифры? (да/нет) ').strip()
+lowercase = input('Включить прописные буквы в пароль? (да/нет) ').strip()
+uppercase = input('Включить строчные буквы в пароль? (да/нет) ').strip()
+punctuations = input('Включить символы, такие как !#$%&*+-=?@^_? в пароль? (да/нет) ').strip()
+bad_symbols = input('Исключить символы il1Lo0O из пароля? (да/нет) ').strip()
 
-password_main()
+if digit.lower() == 'да':
+    chars += digits
+if lowercase.lower() == 'да':
+    chars += lowercase_letters
+if uppercase.lower() == 'да':
+    chars += uppercase_letters
+if punctuations.lower() == 'да':
+    chars += punctuation
+if bad_symbols.lower() == 'да':
+    for i in 'il1Lo0O':
+        chars = chars.replace(i, '')
+
+def generate_password(length, chars):
+    password = ''
+    for i in range(length):
+        password += random.choice(chars)
+    print(password)
+
+for _ in range(num):
+    generate_password(length, chars)
